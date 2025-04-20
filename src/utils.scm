607a392d836f065c651ec1fc415d69382b6c7a6d
koorosh <koorosh.akl@protonmail.com>
2025-04-20 05:08:13 +0300

    util loging small change

--- src/utils.scm
+++ src/utils.scm
@@ -4,7 +4,7 @@
 (define-syntax-rule (lnr expr ...)
   (let ([e (begin expr ...)]
 	[mod-name (module-name (current-module))]
-	[loc (cdar (current-source-location))])
+	[loc (current-source-location)])
     (format (current-output-port) "----- ~a : ~a --> ~a\n" mod-name loc e)
     e))
 (define-syntax isit?
